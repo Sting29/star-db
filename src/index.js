6a754c067ea8073cb113202a31b2d6d7051d8a32
KostyaB <beregovyyk@gmail.com>
2019-09-15 17:22:19 +0300

    created sructure of project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// https://swapi.co
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/';
 
-const getResource = async (url) => {
-    const res = await fetch(url);
-    const body = await res.json();
-    return body;
-};
-
-getResource('https://swapi.co/api/people/1/')
-    .then((body) => {
-        console.log(body);
-    });
+ReactDOM.render(<App />, document.getElementById('root'));
