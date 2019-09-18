a92f7d9b2a6aeb7d9b99631202bb7ea10467b109
KostyaB <beregovyyk@gmail.com>
2019-09-18 11:08:47 +0300

    fixed warning

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -19,11 +19,10 @@ export default class RandomPlanet extends Component {
   componentDidMount() {
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, 10000);
-    // clearInterval(this.interval);
   }
 
   componentWillUnmount() {
-    console.log('will unmount');
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
