1e20c9b283426cf16053ca23fd20f06087b43b8c
KostyaB <beregovyyk@gmail.com>
2019-09-24 12:45:04 +0300

    refactorng HOC and Context

---
+++ src/components/SwComponents/StarshipDetails.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ItemDetails, { Record } from '../ItemDetails/ItemDetails';
+
+import { SwapiServiceConsumer } from '../SwapiServiceContext';
+
+const StarshipDetails = ({ itemId }) => {
+    return (
+        <SwapiServiceConsumer>
+            {
+                ({ getStarship, getStarshipImage }) => {
+                    return (
+                        <ItemDetails 
+                            itemId={itemId}
+                            getData={getStarship}
+                            getImageUrl={getStarshipImage}>
+
+                            <Record field="model" label="Model" />
+                            <Record field="length" label="Length" />
+                            <Record field="costInCredits" label="Cost" />
+                        </ItemDetails>
+                    );
+                }
+            }
+        </SwapiServiceConsumer>
+    );
+};
+
+export default StarshipDetails;
