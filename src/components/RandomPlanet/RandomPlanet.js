880cf512232d443d19e4fa3e71557b8be3ab9b5e
KostyaB <beregovyyk@gmail.com>
2019-10-04 15:48:04 +0300

    add checks with PropTypes and add default props

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Spinner from '../Spinner';
 import ErrorIndicator from '../ErrorIndicator/ErrorIndicator';
@@ -9,6 +10,14 @@ import './RandomPlanet.css';
 
 export default class RandomPlanet extends Component {
 
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
+  static propTypes = {
+    updateInterval: PropTypes.number
+  };
+
   swapiService = new SwapiService();
 
   state = {
@@ -18,8 +27,9 @@ export default class RandomPlanet extends Component {
   };
 
   componentDidMount() {
+    const { updateInterval } = this.props;
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 10000);
+    this.interval = setInterval(this.updatePlanet, updateInterval);
   }
 
   componentWillUnmount() {
